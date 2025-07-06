24762e0821d68d563606378f28338218a2ff1e3b
MLGHassan <70569529+MLGHassan@users.noreply.github.com>
2025-07-07 00:28:37 +0500

    Added retry option and improvements

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 
 function myFunction() {
+
+    // Remove retry button if it exists
+    const retryButton = document.getElementById('retry-button');
+    if (retryButton) retryButton.remove();
+
     const reaction_box = document.getElementById('reaction-box')
     const reaction_text = document.getElementById('reaction-text');
 
@@ -29,12 +34,35 @@ function myFunction() {
         reaction_box.appendChild(reactButton);
         reaction_text.style.height = '0';
 
-        startTime = new Date().getTime();
+        reactButton.style.cursor = 'pointer';
+        reaction_box.style.transition = 'background-color 0.3s ease';
+
+
+        let startTime = new Date().getTime();
         reactButton.onclick = function() {
+            reaction_box.style.backgroundColor = 'skyblue';
+
             const endTime = new Date().getTime();
             const reactionTime = endTime - startTime;
+            reaction_text.style.height = '70%';
             reaction_text.innerHTML = `Your reaction time: ${reactionTime} ms`;
             reactButton.remove();
+
+            const retryButton = document.createElement('button');
+            retryButton.id = 'retry-button';
+            retryButton.style.fontFamily = 'monospace';
+            retryButton.style.fontSize = '1.5em';
+            retryButton.style.color = 'black';
+            retryButton.style.border = 'none';
+            retryButton.style.width = '100%';
+            retryButton.style.height = '30%';
+            retryButton.style.padding = '0';
+            retryButton.style.background = 'none';
+            retryButton.style.cursor = 'pointer';
+            retryButton.textContent = "Try Again";
+            retryButton.onclick = myFunction;
+            reaction_box.appendChild(retryButton);
+
         };
 
     }, Math.floor(Math.random() * 3000) + 2000);
